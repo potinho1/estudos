e8e9c21149855152e7dfab7b8673bd26afd19de7
João vitor gomes <gomesjoaovitor222@gmail.com>
2022-08-29 21:39:46 -0300

    Aula14: Como criar um programa que armazena dados

---
+++ aulas_python/aula_14/Minha_Agenda/contato.py
@@ -0,0 +1,31 @@
+from mailbox import NotEmptyError
+
+
+class Contato:
+    """ Representa um contato"""
+
+    def __init__(self, nome, telefone):
+        """ Atributos"""
+        self.nome = nome
+        self.telefone = telefone
+
+    def info(self):
+        """ Mostra as informações do contato"""
+        print(f"Nome: {self.nome}")
+        print(f"Telefone: {self.telefone}")
+
+    def editar_nome(self):
+        """ Edita o valor associado com o atributo nome do contato"""
+        novo_nome = input(
+            "Entre com o novo nome do contato (ENTER para mnater o nome atual): ")
+        if novo_nome != "":
+            self.nome = novo_nome.title()
+            print("Nome alterado com sucesso!")
+
+    def editar_telefone(self):
+        """ Edita o valor associado com o atributo telefone nome do contato"""
+        novo_tel = input(
+            "Entre com o novo telefone do contato (ENTER para mnater o telefone atual): ")
+        if novo_tel != "":
+            self.telefone = novo_tel
+            print("Telefone alterado com sucesso!")
---
+++ aulas_python/aula_14/Minha_Agenda/minha_agenda.py
@@ -0,0 +1,94 @@
+from contato import Contato
+
+
+class MinhaAgenda:
+    """ Representa uma agenda de contatos telefônicos"""
+
+    def __init__(self, contatos=[]):
+        """ Inicializa os atributos da agenda"""
+        self.contatos = contatos
+
+    def ver_contatos(self):
+        """ Mostra a lista de contatos"""
+        if self.contatos:
+            print("\nLISTA DE CONTATOS\n")
+            for pos, contato in enumerate(self.contatos):
+                print(f"{pos}: {contato.nome}, {contato.telefone}")
+            print("\n")
+        else:
+            print("Não há contatos armazenados na agenda. \n")
+
+    def adicionar_contato(self):
+        """ Cria um novo contato para ser adicionado á lista contatos"""
+        print("\nNOVO CONTATO\n")
+        nome = input("Insira o nome do novo contato: ")
+        telefone = input("Insira o telefone do novo contato: ")
+        novo_contato = Contato(nome.title(), telefone)
+        self.contatos.append(novo_contato)
+        self.contatos.sort(key=lambda contato: contato.nome)
+        print("Contato adicionado com sucesso.\n")
+
+    def excluir_contato(self):
+        """ Exlcui um contato de acordo com sua posição"""
+        if self.contatos:
+            # Mostra a lista de contatos para que o usuário saiba qual contato ele quer excluir
+            self.ver_contatos()
+
+            # Solicita a posição do contato que o usuário deseja excluir
+            pos = int(
+                input("Informa a posição do contato que você deseja apagar: "))
+
+            contato_exluido = self.contatos.pop(pos)
+
+            print(f"{contato_exluido.nome} foi removido da lista contatos.\n")
+        else:
+            print("Não a contatos armazenados na agenda.\n")
+
+    def editar_contato(self):
+        """ Edita dados de um contatos"""
+
+        if self.contatos:
+            # Mostra a lista de contatos para que o usuário saiba a posição do contato que ele deseja editar
+            self.ver_contatos()
+
+            # Solicita a posição para o usuáiro do programa
+            pos = int(
+                input("Informa a posição do contato que você deseja editar: "))
+
+            # Armazena o contato a ser editado na variavel contato_editado
+            contato_editado = self.contatos[pos]
+
+            # Mostra os dados atuais do contato a ser editado
+            contato_editado.info()
+
+            # Editar a informações do contato
+            contato_editado.editar_nome()
+            contato_editado.editar_telefone()
+
+            # Mostra informações atualizadas do contato
+            print("Informações atualizadas")
+            contato_editado.info()
+
+        else:
+            print("Não há contatos armazenados na agenda.\n")
+
+    def run(self):
+        """ Inicia a operação da agenda"""
+        print("MINHA AGENDA")
+        while True:
+            print("1-Ver contatos | 2-Adiconar | 3-Excluir | 4-Editar | 5-Sair")
+            comando = input("Digite o comando desejado: ")
+            if comando in ["1", "2", "3", "4", "5"]:
+                if comando == "1":
+                    self.ver_contatos()
+                elif comando == "2":
+                    self.adicionar_contato()
+                elif comando == "3":
+                    self.excluir_contato()
+                elif comando == "4":
+                    self.editar_contato()
+                else:
+                    print("Até a próxima!")
+                    break
+            else:
+                print("Insira um comando válido!\n")
