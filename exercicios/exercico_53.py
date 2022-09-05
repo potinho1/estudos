8dd6d368d257c967e64091609696276d7e57a7b0
João vitor gomes <gomesjoaovitor222@gmail.com>
2022-09-05 18:50:30 -0300

    Exercícios Python

--- exercicios/exercico_53.py
+++ exercicios/exercico_53.py
@@ -1,6 +1,5 @@
 """Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos. """
-
-from statistics import quantiles
+'''
 
 idades = [18, 19, 20, 21, 22]
 alturas = [1.80, 1.70, 1.90, 1.85, 1.99]
@@ -21,3 +20,24 @@ while c < len(idades):
         quantidade += 1
     c += 1
 print(quantidade)
+
+---------------------------------------------------->
+'''
+
+idades = []
+alturas = []
+
+
+for i in range(30):
+    idade = int(input("Digite sua idade: "))
+    altura = float(input("Digite sua altura: "))
+    idades.append(idade)
+    alturas.append(altura)
+    mediaAltura = sum(alturas)/len(alturas)
+    quantidadeAlunos = 0
+    for i in range(0, len(idades)):
+        if idades[i] > 13 and alturas[i] < mediaAltura:
+            quantidadeAlunos = quantidadeAlunos + 1
+
+print(
+    f"Alunos que possuem mais de 13 anos e altura inferior são: {quantidadeAlunos}")
