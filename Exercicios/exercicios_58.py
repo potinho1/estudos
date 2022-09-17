b5a735130ceef30026fa201610a3c62f981ad01f
Joao vitor <gomesjoaovitor222@gmail.com>
2022-09-17 14:07:27 -0300

    Exercícios Python

--- Exercicios/exercicios_58.py
+++ Exercicios/exercicios_58.py
@@ -44,9 +44,34 @@ while True:
 
 """
 
-Salário (n) = 200 + comissão
-Lista = Salário em intervalos de 100 (0 ~99)​
+num_vendedores = int(input('Quantos vendedores a loja tem? '))
 
-Fórmula (caminho inverso do algoritmo do salário):
+salarios = []
+classe = []
+for vendedor in range(1,num_vendedores+1):
+    vendas = float(input('\nQuanto o vendedor '+str(vendedor)+' arrecadou essa semana? '))
+    salarios.append(200+(vendas*0.09))
 
-Índice do vetor = int ou piso [(salário - 200) ÷ 100]​
+dicionario = {'1':[range(200,300),0],'2':[range(300,400),0],
+              '3':[range(400,500),0],'4':[range(600,700),0],
+              '5':[range(700,800),0],'7':[range(800,900),0],
+              '8':[range(900,1000),0],'9':[range(1000,100000),0]}  
+
+#dicionario.keys() - pega o valor das chaves de cada dicionário
+
+for salario in salarios:
+    print(salario)
+    for idx in dicionario.keys():
+        print(idx)
+        if salario in dicionario[idx][0]:
+            classe.append(idx)
+            dicionario[idx][1] = dicionario[idx][1] + 1     
+            
+#for idx in dicionario.keys():
+#    inicial = dicionario[idx][0][0]
+#    final = dicionario[idx][0][-1]
+#    quant = dicionario[idx][1]
+#    if final == 99999:
+#        print('Número de funcionários com salário acima de R$1000,00 iguai a '+str(quant))
+#    else:
+#        print('Nuúmero de funcionários com salário de R$'+str(inicial)+ ' a R$'+str(final)+' igual a: '+str(quant))
